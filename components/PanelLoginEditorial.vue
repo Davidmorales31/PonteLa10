5701c95363a6b5685440fc7146917d22a814a450
Davidmorales31 <159066984+Davidmorales31@users.noreply.github.com>
2026-07-21 00:25:13 -0500

    style(login): usar imagen dedicada sin logo

--- components/PanelLoginEditorial.vue
+++ components/PanelLoginEditorial.vue
@@ -1,7 +1,7 @@
 <template>
   <aside class="panel-login-editorial" aria-label="Presentacion de cuenta Pont3la10">
     <div class="panel-login-imagen">
-      <img src="/editorial/login_pont3la10_estadio.png" alt="Jugadores celebrando en un estadio internacional">
+      <img src="/editorial/login_pont3la10_estadio_sin_logo.png" alt="Jugadores celebrando en un estadio internacional">
     </div>
     <NuxtLink class="logo-login-superpuesto" to="/" aria-label="Pont3la10 inicio">
       <img src="/brand/pont3la10_logo_login_blanco.png" alt="Pont3la10">
